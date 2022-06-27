9335eae24b99b811318330d73cc1b963a14cf6ea
Diogo Antonny <102389309+DiogoJP202@users.noreply.github.com>
2022-06-26 23:02:45 -0300

    aula 11

---
+++ aula11/ex01.js
@@ -0,0 +1,7 @@
+var velocidade=60.1
+console.log(`A velocidade do seu carro é de ${velocidade}Km/h.`)
+if (velocidade > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Sempre use o seu cinto de seguranca!`)
+
