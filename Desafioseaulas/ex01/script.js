97cfa6d0dfe86d31db18260a630a54564ad94945
Diogo Antonny <102389309+DiogoJP202@users.noreply.github.com>
2022-07-02 13:59:12 -0300

    cor da noite

--- Desafioseaulas/ex01/script.js
+++ Desafioseaulas/ex01/script.js
@@ -3,18 +3,22 @@ function carregar(){
     var hora = agora.getHours()
     var foto = window.document.getElementsByClassName ('imagem')[1]
     var mensagem = window.document.getElementById ('msg')
-    var fundo = window.document.querySelector('body')
+    var fundo = window.document.querySelector ('body')
+    var corbranca = window.document.getElementsByClassName ('branco') [1]
+    var corbranca1 = window.document.getElementsByClassName ('branco') [0]
     mensagem.innerHTML = `Agora são ${hora}h.`
     if (hora >= 0 && hora <= 12) {
         //Bom Dia
         foto.src = 'imagens/manha.png'
-        fundo.style.background = 'cornflowerblue'
+        fundo.style.background = '#a6b5b8'
     } else if(hora >= 13 && hora <= 17) {
         //Boa tarde
         foto.src = 'imagens/tarde.png'
-        fundo.style.background = 'rgb(173, 78, 0)'
+        fundo.style.background = '#e06a0b'
     } else {
         foto.src = 'imagens/noite.png'
-        fundo.style.background = 'rgb(18, 2, 78)'
+        fundo.style.background = '#081b1f'
+        corbranca.style.color = 'white'
+        corbranca1.style.color = 'white'
     }
 }
