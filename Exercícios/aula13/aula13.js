9e6df746b0fdc711894f59d16a858c5fbec77e1f
Diogo Antonny <102389309+DiogoJP202@users.noreply.github.com>
2022-07-10 16:11:47 -0300

    aula 13 e 14

---
+++ Exercícios/aula13/aula13.js
@@ -0,0 +1,22 @@
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+/*
+let a = 1
+while ( a <= 10) 
+{
+    console.log('Tudo bem?')
+    a += 1
+}*/
+var tempo = 0
+do{
+    console.log(`${tempo} segundos!`)
+    tempo += 1
+} while (tempo <= 60)
+if (tempo = 60){
+    tempo = '1 minuto!'
+    console.log(`O tempo de ${tempo} foi atingido!`)
+}
---
+++ Exercícios/aula14/ex14.js
@@ -0,0 +1,17 @@
+/*var a = 1
+do {
+    console.log(a)
+    a = a + 1
+} while(a <= 10)*/
+/*
+for (var a = 1; a <= 10; a = a + 1)
+{
+    console.log(a)
+}*/
+console.log('-Contador-')
+for(var tempo = 1; tempo <= 60 ; tempo += 1 )
+{
+    console.log(`${tempo} segundos!`)
+} if( tempo = 60){
+    console.log('1 minuto!')
+}
