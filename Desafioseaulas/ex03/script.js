fb1008d90b3bae038ed210bc5927d65470040009
Diogo Antonny <102389309+DiogoJP202@users.noreply.github.com>
2022-07-12 17:56:56 -0300

    EXERCICIO FEITO SOZINHOOOOOOOOOOOOOOOOOOOOOO

--- Desafioseaulas/ex03/script.js
+++ Desafioseaulas/ex03/script.js
@@ -3,18 +3,31 @@ var Nfim = document.getElementById ('txtNF')
 var Npassos = document.getElementById ('txtNP')
 var resp = document.getElementById ('res')
 function clicar () {
-if ( Ninicio.value.length == 0 || Nfim.value.length == 0 || Ninicio.value > Nfim.value) {
+if ( Ninicio.value.length == 0 || Nfim.value.length == 0 ) {
     window.alert ('Verifique se as informacões estão corretas!')
 } else {
     var Vinicio = Number(Ninicio.value)
     let Vfim = Number(Nfim.value)
     var Vpassos = Number(Npassos.value) 
-    var parag = document.createElement('p')
-    parag.setAttribute('id', 'contagem')
-    
-        resp.innerHTML = 'Contando:'
-        while(Vinicio < Vfim){
-            parag.innerHTML += `${Vinicio + Vpassos}➡️`
+    if(Vpassos === 0){
+        Vpassos += 1
+    }
+    resp.innerHTML = 'Contando: '
+    if(Vinicio > Vfim){
+        while(Vinicio > Vfim){
+            resp.innerHTML += (`${Vinicio} ➡️`)
+            Vinicio = Vinicio - Vpassos
+        } if ( Vinicio = Vfim ){
+            resp.innerHTML += (`${Vinicio}🏁`)
         }
+    } else {
+        while(Vinicio < Vfim){
+            resp.innerHTML += (`${Vinicio} ➡️`)
+            Vinicio = Vinicio + Vpassos
+        } if ( Vinicio = Vfim ){
+            resp.innerHTML += (`${Vinicio}🏁`)
+        }
+    }
+        
 }
 }
