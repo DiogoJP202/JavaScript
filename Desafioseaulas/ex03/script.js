37e1c984450239da4385262f878477fd0cd7a226
Diogo Antonny <102389309+DiogoJP202@users.noreply.github.com>
2022-07-12 16:56:59 -0300

    desafio 03

--- Desafioseaulas/ex03/script.js
+++ Desafioseaulas/ex03/script.js
@@ -1,18 +1,20 @@
-function carregar(){
-    var Ninicio = document.getElementById ('txtNI')
-    var Nfim = document.getElementById ('txtNF')
-    var Npassos = document.getElementById ('txtNP')
-    var resp = document.getElementById ('res')
-    function clicar() {
-    if (Ninicio.value.length == 0 || Nfim.value.length == 0){
-        window.alert('Verifique se as informacões estão corretas!')
-    } else{
-        var Vinicio = Number(Ninicio.value)
-        let Vfim = Number(Nfim.value)
-        let Vpassos = Number(Npassos.value)
-        while(Vinicio <= Vfim){
-            resp.innerHTML += 1
+var Ninicio = document.getElementById ('txtNI')
+var Nfim = document.getElementById ('txtNF')
+var Npassos = document.getElementById ('txtNP')
+var resp = document.getElementById ('res')
+function clicar () {
+if ( Ninicio.value.length == 0 || Nfim.value.length == 0 || Ninicio.value > Nfim.value) {
+    window.alert ('Verifique se as informacões estão corretas!')
+} else {
+    var Vinicio = Number(Ninicio.value)
+    let Vfim = Number(Nfim.value)
+    var Vpassos = Number(Npassos.value) 
+    var parag = document.createElement('p')
+    parag.setAttribute('id', 'contagem')
+    
+        resp.innerHTML = 'Contando:'
+        while(Vinicio < Vfim){
+            parag.innerHTML += `${Vinicio + Vpassos}➡️`
         }
-    }
 }
 }
