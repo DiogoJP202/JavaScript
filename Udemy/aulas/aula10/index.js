9e325534720d22e7fc462f5978f747fa3b1401b4
Diogo Antonny <102389309+DiogoJP202@users.noreply.github.com>
2022-09-17 20:00:22 -0300

    aulas

---
+++ Udemy/aulas/aula10/index.js
@@ -0,0 +1,6 @@
+let N1 = 15;
+let N2 = 10;
+let N3 = 5.55556;
+console.log((N1.toString())+(N2.toString()));
+console.log(N1.toString(2));
+console.log(N3.toFixed(2));
---
+++ Udemy/aulas/aula11/math.js
@@ -0,0 +1,5 @@
+let N1 = 9.5231;
+let N2 = Math.floor(N1);
+let N3 = Math.ceil(N1)
+let N4 = Math.round(N1)
+console.log(N2,N3,N4); 
