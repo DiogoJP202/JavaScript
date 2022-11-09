485bd8ee95fd3315801ae529b72e9dd40e4150e4
Diogo Antonny <Diogojp202@gmail.com>
2022-11-08 21:00:01 -0300

    aula 46 index2.js

---
+++ Udemy/aulas/aula46/index2.js
@@ -0,0 +1,13 @@
+const pessoas = [
+    {nome: 'Diogo', idade: 18},
+    {nome: 'João', idade: 42},
+    {nome: 'Luiz', idade: 31},
+    {nome: 'Felipe', idade: 15},
+    {nome: 'Will', idade: 27},
+    {nome: 'Daniel', idade: 23}
+];
+
+const pessoaMaisVelha = pessoas.reduce((acumulador, valor) => {
+    if(acumulador.idade > valor.idade) return acumulador;
+    return valor;
+})
