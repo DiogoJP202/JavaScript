7306763de824a6a5f200149c0766fab8ebddeb27
Diogo Antonny <Diogojp202@gmail.com>
2022-11-14 21:22:52 -0300

    aula52

--- Udemy/aulas/aula52/index.js
+++ Udemy/aulas/aula52/index.js
@@ -1,2 +1,6 @@
 const produto1 = {nome: 'banana', preco: 1.5};
 const produto2 = { ...produto1, material: 'madeira'};
+
+Object.freeze(produto2.nome); 
+
+console.log(Object.getOwnPropertyDescriptor(produto2, 'preco'));
