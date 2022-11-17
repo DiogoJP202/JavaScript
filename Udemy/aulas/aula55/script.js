4f9edd654355fa53eb39020603092a1c2ca52263
Diogo Antonny <Diogojp202@gmail.com>
2022-11-16 21:33:41 -0300

    aula55

--- Udemy/aulas/aula55/script.js
+++ Udemy/aulas/aula55/script.js
@@ -15,9 +15,23 @@ function Camisa(nome, preco, cor){
     this.cor = cor;
 };
 
+function Caneca(nome, preco, material, estoque){
+    Produto.call(this, nome, preco);
+    this.material = material;
+    this.estoque = estoque;
+}
+
 Camisa.prototype = Object.create(Produto.prototype);
 Camisa.prototype.constructor = Camisa;
+
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
 
 const camisa01 = new Camisa('Camiseta', 24.99, 'Verde');
 camisa01.desconto(10);
 console.log(camisa01);
+
+const caneca01 = new Caneca('Copinho', 10, 'vidro', 30);
+caneca01.desconto(0.99);
+console.log(caneca01);
