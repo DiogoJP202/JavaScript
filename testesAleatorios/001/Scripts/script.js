342beff1d1f0348174b3e83f71699eb09828fd67
Diogo Antonny <Diogojp202@gmail.com>
2023-03-25 12:11:20 -0300

    javascript
    function sekiroTimer
    function mudaCorSite
    form.addEventListener
    .border

--- testesAleatorios/001/Scripts/script.js
+++ testesAleatorios/001/Scripts/script.js
@@ -1,3 +1,5 @@
+const form = document.querySelector('form');
+
 document.addEventListener("click", event => {
     const element = event.target;
 
@@ -8,17 +10,49 @@ document.addEventListener("click", event => {
 });
 
 function mudaCorSite(color){
-    let corpo = document.getElementsByTagName("body")[0];
-    let fieldset = document.getElementsByTagName("fieldset")[0];
-    let input1 = document.querySelector("#CorSite"); 
+    const corpo = document.getElementsByTagName("body")[0];
+    const main = document.querySelector('main')
+    const fieldset = document.getElementsByTagName("fieldset")[0];
+    const input1 = document.querySelector("#CorSite"); 
+    const borders = document.querySelectorAll('.border');
     let color2;
-
+    
     if(color === "black") color2 = "white" 
     if(color === "white") color2 = "black"
 
     corpo.style.backgroundColor = color;
     corpo.style.color = color2;
+    fieldset.style.backgroundColor = color;
     fieldset.style.borderColor = color2;
     input1.style.borderColor = color2;
+    
+    for(let index in borders){
+        borders[index].style.borderColor = color2;
+    }
+}
+
+function sekiroTimer(){
+    let corpo = document.querySelector("body");
+    let timer = setInterval(()=>{
+        corpo.classList.add('sekiro');
+        setTimeout(() => {
+            corpo.classList.remove('sekiro');
+        }, 3000)
+    }, 60000)
+    
 }
 //faze animacao de presente abrindo
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let formObj = {
+        name: document.querySelector('#INome'),
+        lastName: document.querySelector('Isobrenome'),
+        food: document.querySelector('#Ifood'),
+        color: document.querySelector('#ICor'),
+        animal: document.querySelectorAll('.animal')
+    }
+});
+
+sekiroTimer();
