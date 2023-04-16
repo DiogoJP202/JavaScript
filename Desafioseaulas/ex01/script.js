1867cf17084c0a3cd7f44e68ed00b170458f31bc
Diogo Antonny <Diogojp202@gmail.com>
2023-04-16 08:42:00 -0300

    imagens
    Algumas imagens foram trocadas e planos de fundos mudados para outras cores

--- Desafioseaulas/ex01/script.js
+++ Desafioseaulas/ex01/script.js
@@ -9,14 +9,14 @@ function carregar(){
     mensagem.innerHTML = `Agora são ${hora}h.`
     if (hora >= 0 && hora <= 12) {
         //Bom Dia
-        foto.src = 'imagens/manha.png'
+        foto.src = 'imagens/morning.jpg'
         fundo.style.background = '#a6b5b8'
     } else if(hora >= 13 && hora <= 17) {
         //Boa tarde
-        foto.src = 'imagens/tarde.png'
-        fundo.style.background = '#e06a0b'
+        foto.src = 'imagens/afternoon.jpg'
+        fundo.style.background = '#c8deb6'
     } else {
-        foto.src = 'imagens/noite.png'
+        foto.src = 'imagens/night.jpg'
         fundo.style.background = '#081b1f'
         corbranca.style.color = 'white'
         corbranca1.style.color = 'white'
