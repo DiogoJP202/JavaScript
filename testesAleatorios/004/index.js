d0dafdcafa472a14a82dadcd8ca2ec24dff45320
Diogo Antonny <Diogojp202@gmail.com>
2023-06-06 20:21:43 -0300

    ok

--- testesAleatorios/004/index.js
+++ testesAleatorios/004/index.js
@@ -1,5 +1,39 @@
-[v1, v2, v3] = [0, 3, 5];
-console.log(v1, v2, v3);
+function randomTime(min = 1000, max = 5000){
+	let time = Math.random()*(max-min)-min;
+	return Math.floor(time);
+}
 
-[v1, v2, v3] = [v2, v1, v3];
-console.log(v1, v2, v3);
+function f1(callback){
+	setTimeout(() => {
+		console.log('f1');
+		if(callback) callback();
+	}, randomTime());
+}
+
+function f2(callback){
+	setTimeout(() => {
+		console.log('f2');
+		if(callback) callback();
+	}, randomTime());
+}
+
+function f3(callback){
+	setTimeout(() =>  {
+		console.log('f3');
+		if(callback) callback();
+	}, randomTime());
+}
+
+function f1Callback(){
+	f2(f2Callback);
+}
+
+function f2Callback(){
+	f3(f3Callback);
+}
+
+function f3Callback(){
+	console.log("finished here!")
+}
+
+f1(f1Callback);
