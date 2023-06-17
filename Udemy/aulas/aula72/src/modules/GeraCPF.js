24ba44652a7aa0a16784757f0d4c41e217bad167
Diogo Antonny <Diogojp202@gmail.com>
2023-06-17 13:53:17 -0300

    aula72

---
+++ Udemy/aulas/aula72/src/modules/GeraCPF.js
@@ -0,0 +1,25 @@
+import ValidaCPF from "./ValidaCPF";
+
+export default class  GeraCPF{
+    random(min = 100000000, max= 999999999){
+        return String(Math.floor(Math.random() * (max - min) + min));
+    }
+
+    geraNovoCPF(){
+        const cpfSemDigito = this.random();
+        const digito1 = ValidaCPF.criaDigito(cpfSemDigito);
+        const digito2 = ValidaCPF.criaDigito(cpfSemDigito + digito1);
+        const novoCpf = cpfSemDigito + digito1 + digito2;
+
+        return this.CPFformatado(novoCpf);
+    }
+
+    CPFformatado(cpf){
+        return (
+            cpf.slice(0, 3) + "." +
+            cpf.slice(3, 6) + "." +
+            cpf.slice(6, 9) + "-" +
+            cpf.slice(9, 12)
+        )
+    }
+}
